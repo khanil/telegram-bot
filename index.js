f4ebc0400aa8b931cee3bad2090faa37b694e864
Vyacheslav Khanilo <khaniloslava@yandex.ru>
2020-03-22 15:50:05 +0300

    add express to solve heroku port issue
    telegraf/telegraf#363

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const dotenv = require("dotenv");
+const express = require("express");
 const Telegraf = require("telegraf");
 
 dotenv.config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+// Env variables read
+//
+const port = process.env.PORT || 3000;
+const token = process.env.BOT_TOKEN;
+
+// Express
+//
+const expressApp = express();
+
+expressApp.get("/", (req, res) => {
+  res.send("Working...");
+});
+
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// Telegram Bot
+//
+const bot = new Telegraf(token);
 
 bot.start(ctx => ctx.reply("Welcome"));
 bot.help(ctx => ctx.reply("Send me a sticker"));
